feaaac7760b633f28c052ce0e79d52126728e9be
Aleksandar <aleksandar.naskovich@gmail.com>
2020-04-06 16:09:33 +0200

    Generic button added

--- website/admin_index.php
+++ website/admin_index.php
@@ -13,19 +13,19 @@
       <h2>Admin page</h2>
         <div clas="options">
             <div class="items">
-            <h3>Item settings</h3>
-                <a href="add_item.php"><button>Add item</button></a>
-                <a href="all_categories.php"><button>Edit item</button></a><br><br>
+            <p class="underline">Item settings</p>
+                <a href="add_item.php"><button class="generic_button">Add item</button></a>
+                <a href="all_categories.php"><button class="generic_button">Edit item</button></a><br><br>
             </div>
             <div class="users">
-            <h3>User settings</h3>
-            <a href="#"><button>Add user</button></a>
-            <a href="#"><button>Edit user</button></a>
+            <p class="underline">User settings</p>
+            <a href="#"><button class="generic_button">Add user</button></a>
+            <a href="#"><button class="generic_button">Edit user</button></a>
             </div>
             <div class="settings">
-            <h3>Admin settings</h3>
-            <a href="settings.php"><button>Settings</button></a>
-            <a href="stock_history.php"><button>Stock history</button></a>
+            <p class="underline">Admin settings</p>
+            <a href="settings.php"><button class="generic_button">Settings</button></a>
+            <a href="stock_history.php"><button class="generic_button">Stock history</button></a>
             </div>
         </div>
     </div>
