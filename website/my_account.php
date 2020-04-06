8b25ad3a778229f828acec1b4748f1ffacb00aa8
Milos <knezevicmilos@yahoo.com>
2020-04-06 17:25:47 +0200

    my_account.php

--- website/my_account.php
+++ website/my_account.php
@@ -6,20 +6,6 @@ $s_user = new User_Class();
 if (isset($_SESSION['username'])) {
   $s_user->LoadUser("username", $_SESSION['username']);
 }
-if (isset($_POST["submit"])) {  
-  // Populate user fields
-  $s_user->username       = mysqli_real_escape_string($db, $_POST['username']);
-  $s_user->first_name     = mysqli_real_escape_string($db, $_POST['first_name']);
-  $s_user->last_name      = mysqli_real_escape_string($db, $_POST['last_name']);
-  $s_user->user_email     = mysqli_real_escape_string($db, $_POST['email']);
-  $s_user->user_address   = mysqli_real_escape_string($db, $_POST['user_address']);
-  $s_user->user_phone_no  = mysqli_real_escape_string($db, $_POST['user_phone_no']);
-
-  $s_user->EditUser($s_user->username);
-  
-  
-}
-
 ?>
 <html>
   <head>
@@ -33,13 +19,15 @@ if (isset($_POST["submit"])) {
       <br>
       <h2>My account</h2><br>
       <p class="underline"><?php echo $s_user->first_name; ?>'s info</p><br>
-      <div class = "user_info" style="text-align: left;">
-        <p>Username: <?php echo $s_user->username; ?></p>
-        <p>Full name: <?php echo $s_user->first_name. " ".$s_user->last_name ; ?></p>
-        <p>Email: <?php echo $s_user->user_email; ?></p>
-        <p>Address: <?php echo $s_user->user_address; ?></p>
-        <p>Phone number: <?php echo $s_user->user_phone_no; ?></p>
+      <div class = "user_info">
+        <p>Username: <?php echo "<b>" . $s_user->username ."</b>"; ?></p>
+        <p>Full name: <?php echo "<b>" . $s_user->first_name. " ".$s_user->last_name ."</b>" ; ?></p>
+        <p>Email: <?php echo "<b>" . $s_user->user_email ."</b>"; ?></p>
+        <p>Address: <?php echo "<b>" . $s_user->user_address ."</b>"; ?></p>
+        <p>Phone number: <?php echo "<b>" . $s_user->user_phone_no ."</b>"; ?></p>
+        <a href="update_account_details.php"><button class="generic_button">Edit account details</button></a>
       </div>
+      
          <!-- 
   Purchase history
 -->
@@ -75,87 +63,6 @@ if (isset($_POST["submit"])) {
       } 
     else echo "<b>You don't have any purchase history</b>";
     ?>
-      <!-- 
-  Update form
--->
-      <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data">
-      
-        <p class="underline">Update info</p><br>
-<!-- 
-  Username
--->
-        <div class="FlexContainer">        
-          <div class="col-25">  
-            <label>Username</label>
-          </div>
-          <div class="col-75">
-            <input type="text" name="username" value="<?php echo $s_user->username; ?>" >
-          </div>
-        </div><br>
-<!-- 
-  First name
--->
-<div class="FlexContainer">        
-        <div class="col-25">  
-            <label>First name</label>
-          </div>
-          <div class="col-75">
-            <input type="text" name="first_name" value="<?php echo $s_user->first_name; ?>">
-          </div>
-        </div><br>
-<!-- 
-  Last name
--->
-<div class="FlexContainer">        
-        <div class="col-25">  
-          <label>Last name</label>
-        </div>
-        <div class="col-75">
-         <input type="text" name="last_name" value="<?php echo $s_user->last_name; ?>">
-        </div>
-      </div><br>
-<!-- 
-  Email
--->
-<div class="FlexContainer">
-            <div class="col-25">
-              <label>Email</label>
-            </div>
-            <div class="col-75">
-              <input type="text" name="email" value="<?php echo $s_user->user_email; ?>">
-            </div>
-        </div><br>
-<!-- 
-  User Address
--->
-<div class="FlexContainer">
-            <div class="col-25">
-              <label>User Address</label>
-            </div>
-            <div class="col-75">
-              <input type="text" name="user_address" value="<?php echo $s_user->user_address; ?>" >
-            </div>
-        </div><br>
-<!-- 
-  Phone number
--->
-        <div class="FlexContainer">
-          <div class="col-25">  
-            <label>Phone number</label>
-          </div>
-          <div class="col-75">
-          <input type="text" name="user_phone_no" value="<?php echo $s_user->user_phone_no; ?>">
-          </div>
-        </div><br>
-
-<!-- 
-  Update Button
--->
-          <p class="underline">&nbsp;</p> 
-
-          <button type="submit" class="btn" name="submit">Update</button>       
-
-</form>
     </div>
   </body>
 </html>
