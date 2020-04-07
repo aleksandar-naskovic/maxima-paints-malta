632551a629590658713dfa7163874326154be83b
Aleksandar <aleksandar.naskovich@gmail.com>
2020-04-07 19:14:59 +0200

    Created page change_password.php

---
+++ website/change_password.php
@@ -0,0 +1,90 @@
+<?php
+require_once("../0_core/config.php");
+include("../0_core/session.php");
+
+$s_user = new User_Class();
+if (isset($_SESSION['username'])) {
+  $s_user->LoadUser("username", $_SESSION['username']);
+}
+?>
+<html>
+  <head>
+    <title>Change password</title>
+    <link rel="stylesheet" type="text/css" href="../0_core/style.css"> 
+    <link rel="stylesheet" type="text/css" href="update_account_details.css">   
+  </head>
+  <body>
+    <?php include("navbar.php")?>
+    <div class="page">
+      <br>
+      <h2>Change password</h2><br>
+     
+      <!-- 
+  Update form
+-->
+      <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data">
+      
+        <p class="underline">Change password</p><br>
+<!-- 
+  Old password
+-->
+        <div class="FlexContainer">        
+          <div class="col-25">  
+            <label>Old password</label>
+          </div>
+          <div class="col-75">
+            <input type="password" name="old_password">
+          </div>
+        </div><br>
+<!-- 
+  New password
+-->
+<div class="FlexContainer">        
+        <div class="col-25">  
+            <label>New password</label>
+          </div>
+          <div class="col-75">
+            <input type="password" name="new_password">
+          </div>
+        </div><br>
+<!-- 
+  Confirm new password
+-->
+<div class="FlexContainer">        
+        <div class="col-25">  
+          <label>Confirm new password</label>
+        </div>
+        <div class="col-75">
+         <input type="password" name="confirm_password">
+        </div>
+      </div><br>
+
+<!-- 
+  Update Button
+-->
+          <p class="underline">&nbsp;</p> 
+          <button type="submit" class="btn" name="submit">Update</button> 
+  <!-- 
+  If button clicked
+-->
+  <?php if (isset($_POST["submit"])) { 
+    //check old password
+    if(strcmp($s_user->password, $_POST['old_password']) == 0) {
+      // check if new password match the confirm password
+      if (strcmp($_POST['new_password'], $_POST['confirm_password']) == 0) {
+        $s_user->password = mysqli_real_escape_string($db, $_POST['new_password']);
+        $s_user->ChangePassword($s_user->username);
+      }
+      else echo "<h3>New password doesn't match the confirm password</h3>";
+    }
+    else echo "<h3>Your old password is not correct</h3>";
+ 
+  
+  
+}
+?>      
+
+</form>
+    </div>
+  </body>
+</html>
