5b88ebdb669846fcad02da8bad1596f727909c3a
Aleksandar <aleksandar.naskovich@gmail.com>
2020-04-07 19:08:54 +0200

    included generic button

--- website/admin_index.php
+++ website/admin_index.php
@@ -5,6 +5,7 @@
   <head>
     <title>Admin page</title>
     <link rel="stylesheet" type="text/css" href="../0_core/style.css">
+    <link rel="stylesheet" type="text/css" href="../0_core/generic_input.css">
     <link rel="stylesheet" type="text/css" href="admin_index.css">      
   </head>
   <body>
@@ -14,18 +15,19 @@
         <div clas="options">
             <div class="items">
             <p class="underline">Item settings</p>
-                <a href="add_item.php"><button class="generic_button">Add item</button></a>
-                <a href="all_categories.php"><button class="generic_button">Edit item</button></a><br><br>
+                <a href="add_item.php"><button class="button">Add item</button></a>
+                <a href="all_categories.php"><button class="button">Edit item</button></a><br><br>
             </div>
             <div class="users">
             <p class="underline">User settings</p>
-            <a href="#"><button class="generic_button">Add user</button></a>
-            <a href="#"><button class="generic_button">Edit user</button></a>
+            <a href="#"><button class="button">Add user</button></a>
+            <a href="#"><button class="button">Edit user</button></a>
             </div>
             <div class="settings">
             <p class="underline">Admin settings</p>
-            <a href="settings.php"><button class="generic_button">Settings</button></a>
-            <a href="stock_history.php"><button class="generic_button">Stock history</button></a>
+            <a href="settings.php"><button class="button">Settings</button></a>
+            <a href="stock_history.php"><button class="button">Stock history</button></a>
+            <a href="order_history.php"><button class="button">Order history</button></a>
             </div>
         </div>
     </div>
