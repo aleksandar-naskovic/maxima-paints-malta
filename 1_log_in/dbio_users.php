51417fa47abb8c210ae44afa4e45cead6ccb09ad
Aleksandar <aleksandar.naskovich@gmail.com>
2020-04-08 14:34:02 +0200

    Fixed function CreateUser

--- 1_log_in/dbio_users.php
+++ 1_log_in/dbio_users.php
@@ -22,31 +22,31 @@ class User_Class {
   public $user_phone_no;
   //
   // Get next User_Id Method
-//  protected function GetNextUserId(){
-//    global $db;
-//    //
-//    $v_next_seq = GetSettingValue('next_sequence_user_id');
-//    //
-//    //Check if Sequence exists
-//    if(!empty($v_next_seq)){
-//      //Increment sequence by 1
-//      $v_new_next_seq = number_format($v_next_seq) + 1;
-//      //
-//      //Save new sequence number to Database
-//      if (SetSettingValue('next_sequence_user_id', $v_new_next_seq)){
-//        return $v_next_seq;
-//      }else{
-//        //ERROR: Saving of new next sequence has failed
-//        return '0';
-//      }
-//      //
-//    }else{
-//      //ERROR: Some error happend when getting the sequence or the sequence does not exists
-//      return '0';
-//    }
-//    //
-//  }
-//  //
+  protected function GetNextUserId(){
+    global $db;
+    //
+    $v_next_seq = GetSettingValue('next_sequence_user_id');
+    //
+    //Check if Sequence exists
+    if(!empty($v_next_seq)){
+      //Increment sequence by 1
+      $v_new_next_seq = number_format($v_next_seq) + 1;
+      //
+      //Save new sequence number to Database
+      if (SetSettingValue('next_sequence_user_id', $v_new_next_seq)){
+        return $v_next_seq;
+      }else{
+        //ERROR: Saving of new next sequence has failed
+        return '0';
+      }
+      //
+    }else{
+      //ERROR: Some error happend when getting the sequence or the sequence does not exists
+      return '0';
+    }
+    //
+  }
+  //
   // Load user data
   //
   // p_type = 'user_id'
@@ -98,60 +98,50 @@ class User_Class {
   // Create New User Method
   // User with same "Username" must not already exists
   //
-//  public function CreateUser() {
-//    global $db;
-//    //Get next sequence number
-//    $v_new_seq = $this->GetNextUserId(); 
-//    //
-//    if($v_new_seq > 0){
-//      $query = "INSERT INTO Users
-//                  ( user_id
-//                   ,user_type
-//                   ,user_category
-//                   ,user_status
-//                   ,username
-//                   ,password
-//                   ,first_name
-//                   ,last_name
-//                   ,user_total_spend
-//                   ,user_total_pending
-//                   ,register_date
-//                   ,last_log_on
-//                   ,user_email
-//                   ,user_address
-//                   ,user_phone_no
-//                  )
-//           VALUES ( '$v_new_seq'
-//                   ,'$this->user_type'
-//                   ,'$this->user_category'
-//                   ,'$this->user_status'
-//                   ,'$this->username'
-//                   ,'$this->password'
-//                   ,'$this->first_name'
-//                   ,'$this->last_name'
-//                   ,'$this->user_total_spend'
-//                   ,'$this->user_total_pending'
-//                   ,'$this->register_date'
-//                   ,'$this->last_log_on'
-//                   ,'$this->user_email'
-//                   ,'$this->user_address'
-//                   ,'$this->user_phone_no'
-//                 )";
-//      //Run a query
-//      mysqli_query($db, $query);
-//      //Check if query went fine
-//      if(empty(mysqli_error($db))){
-//        return TRUE;
-//      }else{
-//        return FALSE;
-//      }
-//    }
-//    else // $v_new_seq = 0
-//    {
-//      return FALSE;
-//    }
-//    //
-//  }
+  public function CreateUser() {
+    global $db;
+    //Get next sequence number
+    $v_new_seq = $this->GetNextUserId(); 
+    //
+    if($v_new_seq > 0){
+      $query = "INSERT INTO users
+                  ( user_id
+                   ,user_type
+                   ,username
+                   ,first_name
+                   ,last_name
+                   ,password
+                   ,user_email
+                   ,register_date
+                   ,user_address
+                   ,user_phone_no
+                  )
+           VALUES ( '$v_new_seq'
+                   ,'user'
+                   ,'$this->username'
+                   ,'$this->first_name'
+                   ,'$this->last_name'
+                   ,'$this->password'
+                   ,'$this->user_email'
+                   , NOW()
+                   ,'$this->user_address'
+                   ,'$this->user_phone_no'
+                 )";
+      //Run a query
+      mysqli_query($db, $query);
+      //Check if query went fine
+      if(empty(mysqli_error($db))){
+        return TRUE;
+      }else{
+        return FALSE;
+      }
+    }
+    else // $v_new_seq = 0
+    {
+      return FALSE;
+    }
+    //
+  }
 
 public function EditUser($username){
   global $db;
