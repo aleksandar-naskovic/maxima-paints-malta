af28286c19ff9d108c123361366f1583721c72a9
Aleksandar <aleksandar.naskovich@gmail.com>
2020-07-02 08:55:59 +0200

    website commit

--- website/new_user.php
+++ website/new_user.php
@@ -1,54 +1,54 @@
 <?php
 require_once("../0_core/config.php");
 include("../0_core/session.php");
-	//
-	$b_username_missing = FALSE;
-	$b_first_name_missing = FALSE;
-	$b_last_name_missing = FALSE;
-	$b_password_1_missing = FALSE;
-	$b_reentered_password_missing = FALSE;
-	$b_email_missing = FALSE;
-	$b_address_missing = FALSE;
-	$b_phone_number_missing = FALSE;
-	$b_password_match = FALSE;
-	//
-	//
+  //
+  $b_username_missing = FALSE;
+  $b_first_name_missing = FALSE;
+  $b_last_name_missing = FALSE;
+  $b_password_1_missing = FALSE;
+  $b_reentered_password_missing = FALSE;
+  $b_email_missing = FALSE;
+  $b_address_missing = FALSE;
+  $b_phone_number_missing = FALSE;
+  $b_password_match = FALSE;
+  //
+  //
   if (isset($_POST["reg_user"])) {  
-		//
-		if(empty($_POST["username"])){
-			$b_username_missing = TRUE;
-			//
-		}
-		if(empty($_POST['first_name'])){
-			$b_first_name_missing = TRUE;
-			//
-		}
-		if(empty($_POST['last_name'])){
-			$b_last_name_missing = TRUE;
-			//
-		}
-		if(empty($_POST['password_1'])){
-			$b_password_1_missing = TRUE;
-			//
-		}
-		if(empty($_POST['reentered_password'])){
-			$b_reentered_password_missing = TRUE;
-			//
-		}
-		if(empty($_POST['email'])){
-			$b_email_missing = TRUE;
-			//
-		}
-		if(empty($_POST['address'])){
-			$b_address_missing = TRUE;
-			//
-		}
-		if(empty($_POST['phone_number'])){
-			$b_phone_number_missing = TRUE;
-			//
-		}
-		
-		else {
+    //
+    if(empty($_POST["username"])){
+      $b_username_missing = TRUE;
+      //
+    }
+    if(empty($_POST['first_name'])){
+      $b_first_name_missing = TRUE;
+      //
+    }
+    if(empty($_POST['last_name'])){
+      $b_last_name_missing = TRUE;
+      //
+    }
+    if(empty($_POST['password_1'])){
+      $b_password_1_missing = TRUE;
+      //
+    }
+    if(empty($_POST['reentered_password'])){
+      $b_reentered_password_missing = TRUE;
+      //
+    }
+    if(empty($_POST['email'])){
+      $b_email_missing = TRUE;
+      //
+    }
+    if(empty($_POST['address'])){
+      $b_address_missing = TRUE;
+      //
+    }
+    if(empty($_POST['phone_number'])){
+      $b_phone_number_missing = TRUE;
+      //
+    }
+    
+    else {
     $s_user = new User_Class();
     //
     // Populate item fields
@@ -62,161 +62,161 @@ include("../0_core/session.php");
     $s_user->user_phone_no	     = mysqli_real_escape_string($db, $_POST['phone_number']);
     //
     // Insert the item in the database
-		$s_user->CreateUser();
-		header('location: admin_index.php');
-		}
-	}
+    $s_user->CreateUser();
+    header('location: admin_index.php');
+    }
+  }
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
-	<link rel="stylesheet" type="text/css" href="../0_core/style.css">
-	<link rel="stylesheet" type="text/css" href="../0_core/generic_input.css">
+  <title></title>
+  <link rel="stylesheet" type="text/css" href="../0_core/style.css">
+  <link rel="stylesheet" type="text/css" href="../0_core/generic_input.css">
 </head>
 <body>
-	<?php include("../website/navbar.php"); ?>
-	<div class="page">
-		<h2>Create User</h2>	<br>
-		<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data">
+  <?php include("../website/navbar.php"); ?>
+  <div class="page">
+    <h2>Create User</h2>	<br>
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data">
 <!-- 
   User name
 -->
-			<div class="FlexContainer">
-				<div class="col-25">
-					<label>Username</label>
-				</div>
-				<div class="col-75">	
-					<input type="text" name="username"  value="<?php echo isset($_POST["username"]) ? $_POST["username"] : ''; ?>">
-				</div>
-				<?php 
-				if ($b_username_missing) { 
+      <div class="FlexContainer">
+        <div class="col-25">
+          <label>Username</label>
+        </div>
+        <div class="col-75">	
+          <input type="text" name="username"  value="<?php echo isset($_POST["username"]) ? $_POST["username"] : ''; ?>">
+        </div>
+        <?php 
+        if ($b_username_missing) { 
                 echo '<h5 class="login_warning" >*** Username is missing ***</h5>';
-				}
-				?>
-			</div><br>
+        }
+        ?>
+      </div><br>
 <!-- 
   First name
 -->
-			<div class="FlexContainer">
-				<div class="col-25">
-					<label>First name</label>
-				</div>
-				<div class="col-75">	
-					<input type="text" name="first_name" value="<?php echo isset($_POST["first_name"]) ? $_POST["first_name"] : ''; ?>">
-				</div>
-				<?php 
-				if ($b_first_name_missing) { 
+      <div class="FlexContainer">
+        <div class="col-25">
+          <label>First name</label>
+        </div>
+        <div class="col-75">	
+          <input type="text" name="first_name" value="<?php echo isset($_POST["first_name"]) ? $_POST["first_name"] : ''; ?>">
+        </div>
+        <?php 
+        if ($b_first_name_missing) { 
                 echo '<h5 class="login_warning" >*** First name is missing ***</h5>';
-				}
-				?>
-			</div><br>
+        }
+        ?>
+      </div><br>
 <!-- 
   Last name
 -->
-			<div class="FlexContainer">
-				<div class="col-25">
-					<label>Last name</label>
-				</div>
-				<div class="col-75">	
-					<input type="text" name="last_name" value="<?php echo isset($_POST["last_name"]) ? $_POST["last_name"] : ''; ?>">
-				</div>
-				<?php 
-				if ($b_last_name_missing) { 
+      <div class="FlexContainer">
+        <div class="col-25">
+          <label>Last name</label>
+        </div>
+        <div class="col-75">	
+          <input type="text" name="last_name" value="<?php echo isset($_POST["last_name"]) ? $_POST["last_name"] : ''; ?>">
+        </div>
+        <?php 
+        if ($b_last_name_missing) { 
                 echo '<h5 class="login_warning" >*** Last name is missing ***</h5>';
-				}
-				?>
-			</div><br>
+        }
+        ?>
+      </div><br>
 <!-- 
   Password
 -->			
-			<div class="FlexContainer">
-				<div class="col-25">
-					<label>Password</label>
-				</div>
-				<div class="col-75">	
-					<input type="password" name="password_1">
-				</div>
-				<?php 
-				if ($b_password_1_missing) { 
+      <div class="FlexContainer">
+        <div class="col-25">
+          <label>Password</label>
+        </div>
+        <div class="col-75">	
+          <input type="password" name="password_1">
+        </div>
+        <?php 
+        if ($b_password_1_missing) { 
                 echo '<h5 class="login_warning" >*** Password is missing ***</h5>';
-				}
-				if ($b_password_match) { 
-					echo '<h5 class="login_warning" >*** Passwords are not equal ***</h5>';
-				}
-				?>
-			</div><br>
+        }
+        if ($b_password_match) { 
+          echo '<h5 class="login_warning" >*** Passwords are not equal ***</h5>';
+        }
+        ?>
+      </div><br>
 <!-- 
   Confrim password
 -->			
-			<div class="FlexContainer">
-				<div class="col-25">
-					<label>Confirm password</label>
-				</div>
-				<div class="col-75">	
-					<input type="password" name="reentered_password">
-				</div>
-				<?php 
-				if ($b_reentered_password_missing) { 
+      <div class="FlexContainer">
+        <div class="col-25">
+          <label>Confirm password</label>
+        </div>
+        <div class="col-75">	
+          <input type="password" name="reentered_password">
+        </div>
+        <?php 
+        if ($b_reentered_password_missing) { 
                 echo '<h5 class="login_warning" >*** Reentered password is missing ***</h5>';
-				}
-				?>
-			</div><br>
+        }
+        ?>
+      </div><br>
 <!-- 
   Email
 -->							
-			<div class="FlexContainer">
-				<div class="col-25">
-					<label>Email</label>
-				</div>
-				<div class="col-75">	
-					<input type="email" name="email" value="<?php echo isset($_POST["email"]) ? $_POST["email"] : ''; ?>">
-				</div>
-				<?php 
-				if ($b_email_missing) { 
+      <div class="FlexContainer">
+        <div class="col-25">
+          <label>Email</label>
+        </div>
+        <div class="col-75">	
+          <input type="email" name="email" value="<?php echo isset($_POST["email"]) ? $_POST["email"] : ''; ?>">
+        </div>
+        <?php 
+        if ($b_email_missing) { 
                 echo '<h5 class="login_warning" >*** Email is missing ***</h5>';
-				}
-				?>
-			</div><br>
+        }
+        ?>
+      </div><br>
 <!-- 
   Addres
 -->			
-			<div class="FlexContainer">
-				<div class="col-25">
-					<label>Address</label>
-				</div>
-				<div class="col-75">	
-					<input type="text" name="address" value="<?php echo isset($_POST["address"]) ? $_POST["address"] : ''; ?>">
-				</div>
-				<?php 
-				if ($b_address_missing) { 
+      <div class="FlexContainer">
+        <div class="col-25">
+          <label>Address</label>
+        </div>
+        <div class="col-75">	
+          <input type="text" name="address" value="<?php echo isset($_POST["address"]) ? $_POST["address"] : ''; ?>">
+        </div>
+        <?php 
+        if ($b_address_missing) { 
                 echo '<h5 class="login_warning" >*** Address is missing ***</h5>';
-				}
-				?>
-			</div><br>
+        }
+        ?>
+      </div><br>
 <!-- 
   Phone number
 -->
 
-			<div class="FlexContainer">
-				<div class="col-25">
-					<label>Phone number</label>
-				</div>
-				<div class="col-75">	
-					<input type="text" name="phone_number" value="<?php echo isset($_POST["phone_number"]) ? $_POST["phone_number"] : ''; ?>">
-				</div>
-				<?php 
-				if ($b_phone_number_missing) { 
+      <div class="FlexContainer">
+        <div class="col-25">
+          <label>Phone number</label>
+        </div>
+        <div class="col-75">	
+          <input type="text" name="phone_number" value="<?php echo isset($_POST["phone_number"]) ? $_POST["phone_number"] : ''; ?>">
+        </div>
+        <?php 
+        if ($b_phone_number_missing) { 
                 echo '<h5 class="login_warning" >*** Phone number is missing ***</h5>';
-				}
-				?>
-			</div><br>
-			<!-- 
+        }
+        ?>
+      </div><br>
+      <!-- 
   Submit button
-			-->
-				<button type="submit" class="main_button" name="reg_user">Add new user</button>
-			<br>
-		</form>
-	</div>
+      -->
+        <button type="submit" class="main_button" name="reg_user">Add new user</button>
+      <br>
+    </form>
+  </div>
 </body>
 </html>
